50b84d0e1fb46789c8909be1fecd91190e905eba
omeryilmaz <omer.yilmaz@hesapkurdu.com>
2022-06-25 18:37:37 +0300

    WebAPI / Model validation created for models

--- Infrastructure/Models/Category.cs
+++ Infrastructure/Models/Category.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Infrastructure.Models
 {
     public class Category
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
         public string Name { get; set; }
     }
 }
--- Infrastructure/Models/Seller.cs
+++ Infrastructure/Models/Seller.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Infrastructure.Models
 {
     public class Seller
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
         public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Range(0, 10.0)]
         public double Rating { get; set; }
     }
 }
