fa91eb919a5c9c75feb6f190a8a59107c86b3d1e
omeryilmaz <omer.yilmaz@hesapkurdu.com>
2022-07-02 18:10:58 +0300

    master / ignored files

---
+++ WebAPI/Logs/IWatchLog.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace WebAPI.Logs
+{
+    public interface IWatchLog
+    {
+        Stopwatch watch { get; }
+        long ElapsedMilliseconds { get; }
+        bool IsRunning { get; }
+        void Restart();
+        void Stop();
+    }
+}
---
+++ WebAPI/Logs/WatchLog.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace WebAPI.Logs
+{
+    public class WatchLog : IWatchLog
+    {
+        public WatchLog()
+        {
+            watch = new Stopwatch();
+        }
+        public Stopwatch watch { get; }
+
+        public long ElapsedMilliseconds => watch.ElapsedMilliseconds;
+
+        public bool IsRunning => watch.IsRunning;
+
+
+        public void Restart()
+        {
+            watch.Restart();
+        }
+
+        public void Stop()
+        {
+            watch.Stop();
+        }
+    }
+}
